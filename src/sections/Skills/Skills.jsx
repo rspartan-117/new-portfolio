e6003454d58eb6bdd05e3b740d62cdb139fcfefc
rspartan-117 <riteksharmachd@gmail.com>
2025-04-07 21:32:06 +0530

    fix: added relevant skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -12,11 +12,11 @@ function Skills() {
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
         <SkillList src={checkMarkIcon} skill="TypeScript" />
         <SkillList src={checkMarkIcon} skill="Nodejs" />
+        <SkillList src={checkMarkIcon} skill="Golang" />
+        <SkillList src={checkMarkIcon} skill="REST/GraphQL" />
       </div>
       <hr />
       <div className={styles.skillList}>
@@ -24,21 +24,21 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="Nextjs" />
         <SkillList src={checkMarkIcon} skill="React Native" />
         <SkillList src={checkMarkIcon} skill="Redux" />
-        <SkillList src={checkMarkIcon} skill="Jest" />
+        <SkillList src={checkMarkIcon} skill="Trpc" />
         
       </div>
       <hr />
       <div className={styles.skillList}> 
         <SkillList src={checkMarkIcon} skill="Git" />
-        <SkillList src={checkMarkIcon} skill="Firebase" />
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
-        <SkillList src={checkMarkIcon} skill="Shadcn" />
-        <SkillList src={checkMarkIcon} skill="Storybook" />
+        <SkillList src={checkMarkIcon} skill="PostgreSQL" />
+        <SkillList src={checkMarkIcon} skill="mongodb" />
+        <SkillList src={checkMarkIcon} skill="Next-Auth" />
+        <SkillList src={checkMarkIcon} skill="AWS" />
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Vite" />
-        <SkillList src={checkMarkIcon} skill="LightHouse" />
+        <SkillList src={checkMarkIcon} skill="React-Hook-form" />
+        <SkillList src={checkMarkIcon} skill="Zod" />
         <SkillList src={checkMarkIcon} skill="Tanstack Query" />
         <SkillList src={checkMarkIcon} skill="Progressive Web App" />
         <SkillList src={checkMarkIcon} skill="MicroFrontend" />
