df6d495764f2ec049f186af5be07243fea6bd4f7
ritik-M0 <ritek@m0.ventures>
2025-07-05 13:12:46 +0530

    feat: added new work experience

--- src/sections/Experience/Experience.jsx
+++ src/sections/Experience/Experience.jsx
@@ -25,7 +25,7 @@ function Experience() {
     
      
      <div className='sectionTitleExpdesc'>Built a Chess federation/Club Management platform from scratch.</div> <br/>
-     <div className='sectionTitleExpdesc'> Added admin panels, Payment gateway, Dynamic form and Role Base Access Systems.</div>
+     <div className='sectionTitleExpdesc'> Added admin panels, Payment gateways, Dynamic forms and Role Base Access Systems.</div>
      <br/>
      <div className='sectionTitleExpdesc'>Merged around 150+ commits in one month.</div>
      <hr />
@@ -39,6 +39,32 @@ function Experience() {
         <SkillList src={checkMarkIcon} skill="Zod" />
         <SkillList src={checkMarkIcon} skill="React-Hook-Form" />
         <SkillList src={checkMarkIcon} skill="Next-Auth" />
+        <SkillList src={checkMarkIcon} skill="AWS" />
+        
+      </div>
+      <hr />
+       <hr />
+       <h2 className="sectionTitleExp">Full Stack Engineer (Web2 + Web3) - M0</h2>
+     <hr />
+     <h2 className="sectionTitleExpsub">Product - LifeOrg
+     </h2>
+    
+     
+     <div className='sectionTitleExpdesc'>Developed a lifestyle platform with web3 wallet integrations</div> <br />
+      <div className='sectionTitleExpdesc'>Created a scalable UI architecture using Next.js and styled components </div> <br />
+       <div className='sectionTitleExpdesc'>Architected a maintainable and scalable API backend in NestJS </div>
+     <br/>
+    
+     <hr />
+      <h2 className="sectionTitleExpsub">Tech Stack : </h2>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Nextjs" />
+        <SkillList src={checkMarkIcon} skill="Typescript" />
+        <SkillList src={checkMarkIcon} skill="Nestjs" />
+         <SkillList src={checkMarkIcon} skill="Prisma" />
+         <SkillList src={checkMarkIcon} skill="Postgres" />
+        <SkillList src={checkMarkIcon} skill="Zod" />
+        <SkillList src={checkMarkIcon} skill="Blockchain" />
         <SkillList src={checkMarkIcon} skill="AWS" />
         
       </div>
